8097b26461ef93acb85fd13973ba6600c549b6ee
Rushia-chan <viettholam@gmail.com>
2023-11-14 13:19:44 +0700

    first commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shoplong/admin/');
+define('HTTP_CATALOG', 'http://localhost/shoplong/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shoplong/admin/');
+define('HTTPS_CATALOG', 'http://localhost/shoplong/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/shoplong/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/shoplong/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/shoplong/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/shoplong/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/shoplong/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/shoplong/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/shoplong/system/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/shoplong/system/download/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/shoplong/system/upload/');
+define('DIR_LOGS', 'C:/xampp/htdocs/shoplong/system/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/shoplong/system/modification/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/shoplong/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shoplong');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/shoplong/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/shoplong/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/shoplong/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/shoplong/system/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/shoplong/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/shoplong/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/shoplong/system/config/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/shoplong/image/');
+define('DIR_CACHE', 'C:/xampp/htdocs/shoplong/system/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/shoplong/system/download/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/shoplong/system/upload/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/shoplong/system/modification/');
+define('DIR_LOGS', 'C:/xampp/htdocs/shoplong/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shoplong');
+define('DB_PREFIX', 'oc_');
